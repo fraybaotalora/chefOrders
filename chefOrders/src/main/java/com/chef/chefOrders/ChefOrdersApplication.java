eb5927a8c55cca161fdd881ddebda1b69d720f9e
frayba.otalora <frayba.otalora@globant.com>
2019-10-15 21:45:03 -0500

    adding backend application

---
+++ chefOrders/src/main/java/com/chef/chefOrders/ChefOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.chef.chefOrders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefOrdersApplication.class, args);
+	}
+
+}
---
+++ chefOrders/src/test/java/com/chef/chefOrders/ChefOrdersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chef.chefOrders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChefOrdersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
